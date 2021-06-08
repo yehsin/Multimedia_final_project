32b9ccf404266ac19c226fc85d0eb09aa3ef6195
yehsin <goodcharlie1018@gmail.com>
2021-06-08 22:13:13 +0800

    target buffer is done

--- 2021CS3570_Challenge1_Live_Video_Streaming-master (1)/2021CS3570_Challenge1_Live_Video_Streaming-master/ABR.py
+++ 2021CS3570_Challenge1_Live_Video_Streaming-master (1)/2021CS3570_Challenge1_Live_Video_Streaming-master/ABR.py
@@ -15,19 +15,63 @@ class Algorithm:
         self.SC_slow = 1.0
         self.SC_fast = 1.0
         self.ema_value = []
+
+        #------default setting-----------
+        self.B_min_0 = 0.3
+        self.B_max_0 = 1.0
+        self.B_min_1 = 0.5
+        self.B_max_1 = 2.0
+
+        self.Gamma = 0.95
         
      # Initail
      def Initial(self):
      # Initail your session or something
         self.some_param = 0
-     def ER(self,R,n ,N = 5):
+
+     def ER(self, R, n ,N = 5):
          sum = 0.0
          for i in range(N-1):
              #print(sum)
              sum = sum + abs(R[n-i] - R[n-i-1])
          ERn = abs(R[n] - R[n-N]) / sum
-         #print(ERn)
          return ERn
+
+     # Segment Bitrate Prediction
+     def Seg_Bit_Pre(self, data_size, time_interval):
+         
+         actual_rate = data_size / time_interval
+         self.R.append(self.V_rate * actual_rate) #formula 2
+
+         if(data_size != 0 and self.n > 5) :
+             self.ER_value = self.ER(self.R,self.n,5) #formula 6
+            
+
+             self.SC_fast = 2/ (1 + 5) #formula 4
+             self.SC_slow = 2/ (1 + 50) #formula 5
+
+             self.SC = math.pow((self.ER_value * (self.SC_fast - self.SC_slow) + self.SC_slow),2) #formula 7
+            
+             self.R_hat.append((1-self.SC)*self.R_hat[self.n]  + self.SC * self.R[self.n]) #formual 3
+             self.n = self.n +1
+         return 
+    
+     #formula 8 9
+     def decision_target_BUf(self,Buffer_size):
+         target = 0 
+         Gamma = 0.95
+         if(Buffer_size >= self.B_min_0 and Buffer_size < self.B_max_0):
+             target = 1
+         else:
+             target = 0
+         if(Buffer_size >= 0 and Buffer_size < self.B_min_0):
+             Gamma = 0.95
+         elif(Buffer_size >= self.B_min_1 and Buffer_size < self.B_max_0):
+             Gamma = 1.0
+         else:
+             Gamma = 1.05
+         return (target, Gamma)
+
      # Define your algo
      def run(self, time, S_time_interval, S_send_data_size, S_chunk_len, S_rebuf, S_buffer_size, S_play_time_len,S_end_delay, S_decision_flag, S_buffer_flag,S_cdn_flag,S_skip_time, end_of_video, cdn_newest_id,download_id,cdn_has_frame,IntialVars):
          
@@ -58,38 +102,16 @@ class Algorithm:
              bit_rate = 3
          else:
              bit_rate = 1
-
-         target_buffer = 0
-         latency_limit = 4
-         if(S_send_data_size[-1]!= 0 and S_time_interval[-1]!= 0):
-            actual_rate  = S_send_data_size[-1] / S_time_interval[-1]
-            self.R.append(self.V_rate * actual_rate)
-            #print(actual_rate)
-
-            if(S_send_data_size[-1] != 0 and self.n > 5) :
-                self.ER_value = self.ER(self.R,self.n,5)
-            #print(self.ER_value)
-            #if(S_send_data_size[-1] != 0 and self.n > 2):
-            #    self.ema_value.append(self.EMA(self.n))
-
-            
-
-            self.SC_fast = 2/ (1 + 5)
-            self.SC_slow = 2/ (1 + 50)
-
-            #print(self.R_hat)
-
-            self.SC = math.pow((self.ER_value * (self.SC_fast - self.SC_slow) + self.SC_slow),2)
-            #a = (1-self.SC)*self.R_hat[self.n]  + self.SC * self.R[self.n]
-            self.R_hat.append((1-self.SC)*self.R_hat[self.n]  + self.SC * self.R[self.n])
-            self.n = self.n +1
-            
-            
-            
-
-            
          
-         #print(self.R_hat)
+         #decision targetbuffer and gamma
+         target_buffer, self.Gamma = self.decision_target_BUf(S_buffer_size[-1])
+         #target_buffer = 0
+
+         latency_limit = 4
+
+         if(S_send_data_size[-1]!= 0 and S_time_interval[-1]!= 0):
+            self.Seg_Bit_Pre(S_send_data_size[-1], S_time_interval[-1])
+
          return bit_rate, target_buffer, latency_limit
 
          # If you choose other
