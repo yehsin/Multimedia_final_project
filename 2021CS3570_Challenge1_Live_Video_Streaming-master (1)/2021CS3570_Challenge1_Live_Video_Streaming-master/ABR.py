22bd2dc1d96d6b0c3a57117099a3dc181d8c340e
yehsin <goodcharlie1018@gmail.com>
2021-06-11 20:12:04 +0800

    2021/06/11

--- 2021CS3570_Challenge1_Live_Video_Streaming-master (1)/2021CS3570_Challenge1_Live_Video_Streaming-master/ABR.py
+++ 2021CS3570_Challenge1_Live_Video_Streaming-master (1)/2021CS3570_Challenge1_Live_Video_Streaming-master/ABR.py
@@ -2,6 +2,8 @@
 #NN_MODEL = "./submit/results/nn_model_ep_18200.ckpt" # model path settings, if using ML-based method
 import math
 
+Bitrate = [500.0,850.0,1200.0,1850.0]
+
 class Algorithm:
      def __init__(self):
      # fill your self params
@@ -15,6 +17,10 @@ class Algorithm:
         self.SC_slow = 1.0
         self.SC_fast = 1.0
         self.ema_value = []
+        self.current_bitrate = 500.0
+        self.predict_time = []
+        self.last_cdn_idx = 0
+        
 
         #------default setting-----------
         self.B_min_0 = 0.3
@@ -41,7 +47,9 @@ class Algorithm:
      def Seg_Bit_Pre(self, data_size, time_interval):
          
          actual_rate = data_size / time_interval
-         self.R.append(self.V_rate * actual_rate) #formula 2
+         #print(actual_rate / 1024.0)
+         self.R.append(self.V_rate * actual_rate/1024.0) #formula 2
+         
 
          if(data_size != 0 and self.n > 5) :
              self.ER_value = self.ER(self.R,self.n,5) #formula 6
@@ -71,6 +79,58 @@ class Algorithm:
          else:
              Gamma = 1.05
          return (target, Gamma)
+     
+     def network_throughput(self,time_interval,data,n):
+         init = 0.0
+         sum = 0.0
+         for i in range(n):
+             init = init + i
+         weight = 1.0 / init
+         #print(weight)
+         for j in range(n):
+             #print(data[j])
+             sum = sum + (data[j] / time_interval[j]) * weight * j
+         total = n * (n+1) / 2.0
+         #print(sum)
+         return sum
+
+
+     def downloading_time(self,r,d,time_interval,data,slice):
+         throughput = self.network_throughput(time_interval,data,slice)
+         return (r * d) / throughput #formula 10
+    
+     def predict_V(self, beta, cdn_idx , d, cur_predict_time):
+         return beta * ((cdn_idx - self.last_cdn_idx) * d / cur_predict_time)
+
+     def Bitrate_control(self, r, d, time_interval , data, slice, buffer_size, cdn_idx, dld, time):
+
+         bitrates = []
+         D_cdn = []
+         tmp_cdn = []
+         threhold = buffer_size
+
+         for i in range(len(Bitrate)):
+
+             bitrates.append(self.R_hat[-1] * self.current_bitrate / Bitrate[i])
+
+             predict_time = self.downloading_time(bitrates[-1],d,time_interval[-slice:],data[-slice:],slice)
+         
+             next_buffer_size = max(buffer_size + d - predict_time * self.Gamma, 0)
+
+             next_V = self.predict_V(beta = 1.0, cdn_idx = cdn_idx, d=d, cur_predict_time = time)
+         
+             Distance = max((cdn_idx - dld) * d + next_V * predict_time - d, 0 )
+
+             D = next_buffer_size + Distance
+
+             D_cdn.append(D)
+
+             if(next_buffer_size > threhold):
+                 tmp_cdn.append(D)
+         #print(tmp_cdn)
+         idx = D_cdn.index(min(tmp_cdn))
+
+         return idx
 
      # Define your algo
      def run(self, time, S_time_interval, S_send_data_size, S_chunk_len, S_rebuf, S_buffer_size, S_play_time_len,S_end_delay, S_decision_flag, S_buffer_flag,S_cdn_flag,S_skip_time, end_of_video, cdn_newest_id,download_id,cdn_has_frame,IntialVars):
@@ -89,10 +149,11 @@ class Algorithm:
          return bit_rate, target_buffer'''
 
 
-
          # If you choose BBA
          RESEVOIR = 0.5
          CUSHION =  1.5
+
+         new_bitrate = 0
          
          if S_buffer_size[-1] < RESEVOIR:
              bit_rate = 0    
@@ -106,18 +167,27 @@ class Algorithm:
          #decision targetbuffer and gamma
          target_buffer, self.Gamma = self.decision_target_BUf(S_buffer_size[-1])
          #target_buffer = 0
-
          latency_limit = 4
 
          if(S_send_data_size[-1]!= 0 and S_time_interval[-1]!= 0):
+
+            download_time = S_send_data_size[-1] / S_time_interval[-1]
+
             self.Seg_Bit_Pre(S_send_data_size[-1], S_time_interval[-1])
+
+            bit_rate = self.Bitrate_control(r=self.R_hat[-1], d=S_chunk_len[-1], data=S_send_data_size,time_interval = S_time_interval ,slice=5, buffer_size=S_buffer_size[-1], cdn_idx=cdn_newest_id, dld=download_id, time=download_time)
+
+         self.last_cdn_idx = cdn_newest_id
+         
+         self.current_bitrate = Bitrate[bit_rate]
+
+         #print(self.current_bitrate)
+         #print(bit_rate, new_bitrate)
 
          return bit_rate, target_buffer, latency_limit
 
          # If you choose other
          #......
-
-
 
      def get_params(self):
      # get your params
